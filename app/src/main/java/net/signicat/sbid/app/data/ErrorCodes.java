e05e587d54bd6a47292bccafb67faf43a65d8312
Martin Syvertsen <marsyv@signicat.com>
2014-05-07 21:45:45 +0200

    Added another error code

--- app/src/main/java/net/signicat/sbid/app/data/ErrorCodes.java
+++ app/src/main/java/net/signicat/sbid/app/data/ErrorCodes.java
@@ -7,12 +7,12 @@ public class ErrorCodes {
     public static final HashMap<String, String> errorHashMap;
     static{
         errorHashMap = new HashMap<String, String>();
-        //Empty (null) messages means that the user is not to se the error
         errorHashMap.put("INVALID_PARAMETERS", null);
         errorHashMap.put("ALREADY_IN_PROGRESS", "RFA3");
         errorHashMap.put("INTERNAL_ERROR", "RFA5");
         errorHashMap.put("RETRY", "RFA5");
         errorHashMap.put("ACCESS_DENIED_RP", null);
+        errorHashMap.put("Internal system error", null);
     }
 
 }
